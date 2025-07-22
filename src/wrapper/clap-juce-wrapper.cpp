8d670fb1406527b7b31a57cba63b0d2ef8c6da45
jatin <jatinchowdhury18@gmail.com>
2025-07-22 11:03:59 -0700

    jbt

--- src/wrapper/clap-juce-wrapper.cpp
+++ src/wrapper/clap-juce-wrapper.cpp
@@ -1822,7 +1822,7 @@ class ClapJuceWrapper : public clap::helpers::Plugin<
                 }
                 else
                 {
-                    FIXME("Handle bypass and deactivated states")
+                    // FIXME("Handle bypass and deactivated states")
                     processor->processBlock(buffer, midiBuffer);
                 }
             }
@@ -2169,7 +2169,7 @@ class ClapJuceWrapper : public clap::helpers::Plugin<
             }
         }
 
-        void paint(juce::Graphics &g) override { g.fillAll(juce::Colours::red); }
+        void paint(juce::Graphics &g) override { g.fillAll(juce::Colours::black); }
         void resized() override
         {
             if (editor != nullptr)
