8214876a92ba091ad4d5dd72059e6bb4ff817f2a
jatin <jatinchowdhury18@gmail.com>
2026-09-08 14:03:49 -0400

    Release resources

--- src/wrapper/clap-juce-wrapper.cpp
+++ src/wrapper/clap-juce-wrapper.cpp
@@ -936,6 +936,7 @@ class ClapJuceWrapper : public clap::helpers::Plugin<
 
     void deactivate() noexcept override
     {
+        processor->releaseResources();
         if (processorAsClapProperties)
             processorAsClapProperties->is_clap_active = false;
     }
